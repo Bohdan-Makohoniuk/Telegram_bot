0543c21196cf22b503da049c2a2fdd257a30228b
Bohdan <103361804+Bohdan-Makohoniuk@users.noreply.github.com>
2023-05-10 09:17:49 +0300

    ChatGPT_API

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
-import { Telegraf } from "telegraf";
-import config from "config";
+import { Telegraf } from 'telegraf';
+import { message } from 'telegraf/filters';
+import config from 'config';
 
-const bot = new Telegraf(config.get("TELEGRAM_TOKEN"));
+const bot = new Telegraf(config.get('TELEGRAM_TOKEN'));
+bot.on(message('text'), async ctx => {
+  await ctx.reply();
+});
+bot.command('start', async ctx => {
+  await ctx.reply();
+});
 
 bot.launch();
 
-process.once("SIGINT", () => bot.stop("SIGINT"));
-process.once("SIGTERM", () => bot.stop("SIGTERM"));
-
-{
-  // https://www.youtube.com/watch?v=-6ufFPvp6CY  (time 9:54)
-}
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
